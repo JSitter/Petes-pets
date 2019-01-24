a427bb5ce4266a0201c36c9247a460b7bc6fc8d9
Justin Sitter <justin@jaytria.com>
2019-01-23 19:46:41 -0800

    add pagination

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,16 @@ const Pet = require('../models/pet');
 
 module.exports = (app) => {
 
-  /* GET home page. */
-//   app.get('/', (req, res) => {
-//     Pet.find().exec((err, pets) => {
-//       res.render('pets-index', { pets: pets });    
-//     });
-//   });
-// }
-
 app.get('/', (req, res) => {
   Pet.paginate({}, {limit:2}, (err, results) => {
     console.log(results)
     console.log(results.pages)
-    res.render('pets-index', { pets: results.docs , pages: results.pages });    
+    pages = []
+    for (i = 1; i <= results.pages; i++){
+      pages.push(i)
+    }
+    console.log(pages)
+    res.render('pets-index', { pets: results.docs , pages: pages, cur_page: results.page });    
   });
 });
 }
