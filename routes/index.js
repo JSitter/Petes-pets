d3640e08f509ad39eaa4d4c5eebfebe85d2fa08e
Justin Sitter <justin@jaytria.com>
2019-01-28 14:56:12 -0800

    Implemented validation

--- routes/index.js
+++ routes/index.js
@@ -14,16 +14,16 @@ app.get('/', (req, res) => {
 
 app.get('/search', (req, res) => {
   term = new RegExp(req.query.term, 'i')
-  
+  const page = req.query.page || 1
   Pet.paginate({$or:[
     {'name': term},
     {'species': term}
-  ]}, {limit:3}, (err, results) => {
+  ]}, {page: page, limit:10}, (err, results) => {
     pages = []
     for (i = 1; i <= results.pages; i++){
       pages.push(i)
     }
-    res.render('pets-index', { pets: results.docs , pages: pages, cur_page: results.page });    
+    res.render('pets-index', { pets: results.docs , pages: pages, cur_page: page });    
   });
 });
 
