c446e9055dc4b976739d7dcf1de73b743ed3d18f
Justin Sitter <justin@jaytria.com>
2019-01-28 13:23:36 -0800

    add search by pet name

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,22 @@ module.exports = (app) => {
 
 app.get('/', (req, res) => {
   Pet.paginate({}, {limit:2}, (err, results) => {
-    console.log(results)
-    console.log(results.pages)
     pages = []
     for (i = 1; i <= results.pages; i++){
       pages.push(i)
     }
-    console.log(pages)
     res.render('pets-index', { pets: results.docs , pages: pages, cur_page: results.page });    
   });
 });
+
+app.get('/search', (req, res) => {
+  Pet.paginate({name:req.query.term}, {limit:3}, (err, results) => {
+    pages = []
+    for (i = 1; i <= results.pages; i++){
+      pages.push(i)
+    }
+    res.render('pets-index', { pets: results.docs , pages: pages, cur_page: results.page });    
+  });
+});
+
 }
